63dd62ec54cc132199eadd22a6c22bc79a360137
Yogurt7v <egorov-2k@yandex.ru>
2026-01-19 20:58:50 +0300

    first commit

---
+++ src/data/projects.ts
@@ -0,0 +1,5 @@
+export const projects = [
+  { slug: 'coffee-site', title: 'Сайт кофейни', tech: 'React' },
+  { slug: 'fitness-app', title: 'Фитнес приложение', tech: 'Astro' },
+  { slug: 'photo-portfolio', title: 'Портфолио фотографа', tech: 'Astro' },
+];
