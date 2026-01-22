6c4ce8dcaf7fbc9cb1bf98f000ba6ffefdb6cf6c
Yogurt7v <egorov-2k@yandex.ru>
2026-01-22 19:49:52 +0300

    правка импортов

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Portfolio } from './Portfolio';
+export { default as Timeline } from './Timeline';
+export { default as Footer } from './Footer';
+export { default as Skills } from './Skills';
+export { default as Header } from './Header';
+export { default as About } from './About';
