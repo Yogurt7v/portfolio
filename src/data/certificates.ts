452907927bd51823f05ede0ae43168c73c7554fb
Yogurt7v <egorov-2k@yandex.ru>
2026-02-05 20:45:05 +0300

    правки наполнения

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -7,24 +7,24 @@ export const certificates = [
   },
   {
     id: 2,
-    name: 'Junior Fronted-разработчик',
-    source: '/сertificate/junior2.webp',
-    issuer: 'Result University',
-    date: '08-05-2024',
-  },
-  {
-    id: 3,
     name: 'Angular',
     source: '/сertificate/angular.webp',
     issuer: 'Sololearn',
     date: '07-06-2024',
   },
   {
-    id: 4,
+    id: 3,
     name: 'JavaScrypt Intermediate',
     source: '/сertificate/js-int.webp',
     issuer: 'Sololearn',
     date: '08-07-2024',
+  },
+  {
+    id: 4,
+    name: 'Junior Fronted-разработчик',
+    source: '/сertificate/junior2.webp',
+    issuer: 'Result University',
+    date: '08-05-2024',
   },
   {
     id: 5,
